0ccf866505665af37c58c1eeb89f3e20ed6114a6
pidokige02 <pidokige0204@gmail.com>
2023-05-03 20:03:16 +0900

    make  realserver is accessable to vue client

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://hobbycoding.shop'  // accessing real server address
 
 createApp(App).use(store).use(router, axios).mount('#app')
