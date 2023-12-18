31caa0e9430ec3bc9a29326fa42e64cbd9289ce7
pidokige02 <pidokige0204@gmail.com>
2023-12-18 19:27:00 +0900

    sync up w/ server env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import store from './store'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://127.0.0.1:8000'
-axios.defaults.baseURL = 'https://hobbycoding.shop'  // accessing real server address
+axios.defaults.baseURL = 'https://shop.hobbycoding.site'  // accessing real server address
 
 createApp(App).use(store).use(router, axios).mount('#app')
